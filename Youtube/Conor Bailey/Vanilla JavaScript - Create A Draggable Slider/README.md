f35eace077844a67a5c7a00c5e5e09a32e93d1b3
DreamEcho100 <maze6572198@gmail.com>
2021-12-27 21:31:21 +0200

    project up to date

---
+++ Youtube/Conor Bailey/Vanilla JavaScript - Create A Draggable Slider/README.md
@@ -0,0 +1,5 @@
+# Credit
+
+Also thanks to:
+
+- [Ethan Hughes article](https://morioh.com/p/6dfe3a99ed4b)
--- Youtube/Conor Bailey/Vanilla JavaScript - Create A Draggable Slider/app.js
+++ Youtube/Conor Bailey/Vanilla JavaScript - Create A Draggable Slider/app.js
@@ -1,69 +1,185 @@
-const slider = document.querySelector('.slider');
+const outerSlider = document.querySelector('.slider');
 const innerSlider = document.querySelector('.slider-inner');
 
-let pressed = false;
-let oldXPos = 0;
-let currXPos = 0;
+let sliderDragAnimationID;
+let isDragging = false;
+let oldXTranslate = 0;
+let currXTranslate = 0;
 let sliderXPos = 0;
 
-slider.addEventListener(
-	/*'mousedown'*/ 'pointerdown' /*'touchstart'*/,
-	(event) => {
-		pressed = true;
-		oldXPos = getPositionX(event);
-		slider.style.cursor = 'grabbing';
-	}
+let outerSliderCoordination;
+let firstSliderCoordination;
+let lastSliderCoordination;
+
+const touchStart = (event) => {
+	isDragging = true;
+	oldXTranslate = getPositionX(event);
+	outerSlider.style.cursor = 'grabbing';
+};
+
+const touchEnd = () => {
+	isDragging = false;
+	outerSlider.style.cursor = 'grab';
+	cancelAnimationFrame(sliderDragAnimationID);
+};
+
+const touchMove = (event) => {
+	event.preventDefault();
+	if (!isDragging || checkSliderBoundary(getPositionX(event) - oldXTranslate))
+		return;
+
+	currXTranslate = getPositionX(event);
+
+	sliderXPos += currXTranslate - oldXTranslate;
+
+	sliderDragAnimationID = requestAnimationFrame(sliderDragAnimation);
+
+	oldXTranslate = currXTranslate;
+};
+
+outerSlider.addEventListener(
+	// 'pointerdown'
+	// 'touchstart'
+	'mousedown',
+	touchStart
+);
+outerSlider.addEventListener(
+	// 'pointerup'
+	// 'touchend'
+	'mouseup',
+	touchEnd
+);
+outerSlider.addEventListener(
+	// 'pointerleave'
+	// 'touchend'
+	'mouseleave',
+	touchEnd
+);
+outerSlider.addEventListener(
+	// 'pointermove'
+	// 'touchmove'
+	'mousemove',
+	touchMove
 );
 
-slider.addEventListener(/*'mouseup'*/ 'pointerup' /*'touchend'*/, (event) => {
-	slider.style.cursor = 'grab';
-});
-
-window.addEventListener(/*'mouseup'*/ 'pointerup' /*'touchend'*/, (event) => {
-	pressed = false;
-	slider.style.cursor = 'grab';
-});
-slider.addEventListener(
-	/*'mousemove'*/ 'pointermove' /*'touchmove'*/,
-	(event) => {
-		event.preventDefault();
-		if (!pressed || checkSliderBoundary(getPositionX(event) - oldXPos)) return;
-
-		currXPos = getPositionX(event);
-
-		sliderXPos += currXPos - oldXPos;
-
-		innerSlider.style.transform = `translateX(${sliderXPos}px)`;
-
-		oldXPos = currXPos;
-	}
-);
+outerSlider.addEventListener('touchstart', touchStart);
+outerSlider.addEventListener('touchend', touchEnd);
+outerSlider.addEventListener('touchend', touchEnd);
+outerSlider.addEventListener('touchmove', touchMove);
 
 const getPositionX = (event) =>
-	event.pointerType === 'mouse' ? event.pageX : event.clientX;
-
-let outerSliderCoordination;
-let innerSliderCoordination;
+	// event.pointerType === 'mouse' ? event.pageX : event.clientX;
+	event.type.includes('mouse') ? event.pageX : event.touches[0].clientX;
 
 const checkSliderBoundary = (num) => {
-	outerSliderCoordination = slider.getBoundingClientRect();
-	innerSliderCoordination =
-		innerSlider.children[
-			innerSlider.children.length - 1
-		].getBoundingClientRect();
+	outerSliderCoordination = outerSlider.getBoundingClientRect();
 
-	if (
-		(num < 0 &&
-			innerSliderCoordination.left +
-				innerSliderCoordination.width * 0.95 +
-				num <
-				outerSliderCoordination.left) ||
-		(num > 0 &&
-			innerSliderCoordination.x + num >
-				outerSliderCoordination.right - outerSliderCoordination.width * 0.05) // Right boundary Check
-	) {
-		return true;
+	if (num > 0) {
+		firstSliderCoordination = innerSlider.children[0].getBoundingClientRect();
+
+		if (
+			firstSliderCoordination.left +
+				firstSliderCoordination.width * 0.05 +
+				num >
+			outerSliderCoordination.right
+		)
+			return true;
+	} else if (num < 0) {
+		lastSliderCoordination =
+			innerSlider.children[
+				innerSlider.children.length - 1
+			].getBoundingClientRect();
+
+		if (
+			lastSliderCoordination.right - lastSliderCoordination.width * 0.05 + num <
+			outerSliderCoordination.left
+		)
+			return true;
 	}
 
 	return false;
 };
+
+const sliderDragAnimation = () => {
+	setSliderPosition();
+	if (isDragging) requestAnimationFrame(sliderDragAnimation);
+};
+
+const setSliderPosition = () => {
+	innerSlider.style.transform = `translateX(${sliderXPos}px)`;
+};
+
+/*
+const slider = document.querySelector('.slider-inner'),
+	slides = Array.from(outerSlider.children);
+let isDragging = false,
+	startPos = 0,
+	currentTranslate = 0,
+	prevTranslate = 0,
+	animationID,
+	currentIndex = 0;
+slides.forEach((slide, index) => {
+	// const slideImage = slide.querySelector('i');
+	// disable default image drag
+	slide.isDragging = true;
+	slide.addEventListener('dragstart', (e) => e.preventDefault());
+	// touch events
+	slide.addEventListener('touchstart', touchStart(index));
+	slide.addEventListener('touchend', touchEnd);
+	slide.addEventListener('touchmove', touchMove);
+	// mouse events
+	slide.addEventListener('mousedown', touchStart(index));
+	slide.addEventListener('mouseup', touchEnd);
+	slide.addEventListener('mousemove', touchMove);
+	slide.addEventListener('mouseleave', touchEnd);
+});
+// make responsive to viewport changes
+window.addEventListener('resize', setPositionByIndex);
+// prevent menu popup on long press
+window.oncontextmenu = function (event) {
+	event.preventDefault();
+	event.stopPropagation();
+	return false;
+};
+function getPositionX(event) {
+	return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX;
+}
+function touchStart(index) {
+	return function (event) {
+		currentIndex = index;
+		startPos = getPositionX(event);
+		isDragging = true;
+		animationID = requestAnimationFrame(animation);
+		outerSlider.classList.add('grabbing');
+	};
+}
+function touchMove(event) {
+	if (isDragging) {
+		const currentPosition = getPositionX(event);
+		currentTranslate = prevTranslate + currentPosition - startPos;
+	}
+}
+function touchEnd() {
+	cancelAnimationFrame(animationID);
+	isDragging = false;
+	const movedBy = currentTranslate - prevTranslate;
+	// if moved enough negative then snap to next slide if there is one
+	if (movedBy < -100 && currentIndex < slides.length - 1) currentIndex += 1;
+	// if moved enough positive then snap to previous slide if there is one
+	if (movedBy > 100 && currentIndex > 0) currentIndex -= 1;
+	setPositionByIndex();
+	outerSlider.classList.remove('grabbing');
+}
+function animation() {
+	setSliderPosition();
+	if (isDragging) requestAnimationFrame(animation);
+}
+function setPositionByIndex() {
+	currentTranslate = currentIndex * -window.innerWidth;
+	prevTranslate = currentTranslate;
+	setSliderPosition();
+}
+function setSliderPosition() {
+	outerSlider.style.transform = `translateX(${currentTranslate}px)`;
+}
+*/
